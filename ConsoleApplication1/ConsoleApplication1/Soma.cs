85148635f164a45793e6a0b1c8f5bedad807b22b
Orismar <orismarfh@gmail.com>
2016-04-05 16:08:22 -0300

    Treino com classes

--- ConsoleApplication1/ConsoleApplication1/Soma.cs
+++ ConsoleApplication1/ConsoleApplication1/Soma.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     class Soma
     {
         public int Param1 { get; set; }
-        public int Param2 { get; set; }
+       // public int Param2 { get; set; }
         public int Total { get; set; }
 
         public int Somar(int Param1, int Param2)
