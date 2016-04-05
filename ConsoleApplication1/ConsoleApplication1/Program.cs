fe42312dccc1d5f04739bc54b38537ff49d1521d
Orismar <orismarfh@gmail.com>
2016-04-04 22:36:13 -0300

    Treino com classes e metodos/propriedades

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Soma();
+            var dog2 = new Soma();
+            dog.Somar(2, 2);
+            dog2.Somar(5, 5);
+            
+            string strx = dog.Total.ToString();
+            string stry = dog2.Total.ToString();
+
+            Console.WriteLine(strx);
+            Console.WriteLine(stry);
+            Console.ReadLine();
+
+        }
+    }
+}
